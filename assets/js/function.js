a3e63be8f338ffc067c347abc4027bca2b8a04df
JoelDavid7324 <159042638+JoelDavid7324@users.noreply.github.com>
2024-04-03 10:06:10 -0400

    "Added functionality to get input value and update timer display on button click"
    This commit adds code to get the value of the input field where the user specifies the time for the timer. It also adds an event listener to the "Iniciar" button, so that when it is clicked, the timer display is updated with the value from the input field.
    
    Additionally, the commit changes the "Iniciar" and "Pausar" buttons from simple HTML buttons to JavaScript objects, so that they can have event listeners added to them.

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,4 +1,4 @@
-const verificarEntero = document.getElementById("tiempo__input");
+const verificarEntero = document.querySelector(".tiempo__input");
 
 verificarEntero.addEventListener("keydown", (event) => {
   const valido =
@@ -7,9 +7,37 @@ verificarEntero.addEventListener("keydown", (event) => {
     event.key === "ArrowLeft" ||
     event.key === "ArrowRight" ||
     event.key === "Delete";
-  const superaLongitudMaxima = numeroInput.value.length >= 11;
+  const superaLongitudMaxima = verificarEntero.value.length >= 11;
 
   if (!valido || superaLongitudMaxima) {
     event.preventDefault();
   }
+});
+
+const iniciarBotonnn = document.querySelector(".iniciarBoton");
+const pausarBoton = document.querySelector(".pausarBoton");
+let pTiempoRestante = document.querySelector(".temporizador--rtiempo");
+
+iniciarBotonnn.addEventListener("click", () => {
+
+  let minutosI = verificarEntero.value;
+  let segundosTotales = minutosI * 60;
+
+  let intervalo = setInterval(() => {
+    segundosTotales--;
+
+    iniciarBotonnn.disabled = true; 
+
+    let horas = Math.floor(segundosTotales / 3600);
+    let minutos = Math.floor((segundosTotales % 3600) / 60);
+    let segundos = segundosTotales % 60;
+
+    horas = horas < 10 ? "0" + horas : horas;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+    pTiempoRestante.textContent = `${horas}:${minutos}:${segundos}`;
+    if (segundosTotales === 0) {
+      clearInterval(intervalo);
+    }
+  }, 1000);
 });
