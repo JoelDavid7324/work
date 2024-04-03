a9664526819e098f4ac73bcee2cf449d40919bb6
JoelDavid7324 <159042638+JoelDavid7324@users.noreply.github.com>
2024-04-03 11:39:49 -0400

    ultimo cambio

--- assets/js/function.js
+++ assets/js/function.js
@@ -14,19 +14,21 @@ verificarEntero.addEventListener("keydown", (event) => {
   }
 });
 
-const iniciarBotonnn = document.querySelector(".iniciarBoton");
+const iniciarBoton = document.querySelector(".iniciarBoton");
 const pausarBoton = document.querySelector(".pausarBoton");
 let pTiempoRestante = document.querySelector(".temporizador--rtiempo");
 
-iniciarBotonnn.addEventListener("click", () => {
+let tiempoPausado = 0;
 
+iniciarBoton.addEventListener("click", () => {
   let minutosI = verificarEntero.value;
   let segundosTotales = minutosI * 60;
+  //let bool = true;
 
   let intervalo = setInterval(() => {
     segundosTotales--;
 
-    iniciarBotonnn.disabled = true; 
+    iniciarBoton.disabled = true;
 
     let horas = Math.floor(segundosTotales / 3600);
     let minutos = Math.floor((segundosTotales % 3600) / 60);
@@ -35,9 +37,53 @@ iniciarBotonnn.addEventListener("click", () => {
     horas = horas < 10 ? "0" + horas : horas;
     minutos = minutos < 10 ? "0" + minutos : minutos;
     segundos = segundos < 10 ? "0" + segundos : segundos;
-    pTiempoRestante.textContent = `${horas}:${minutos}:${segundos}`;
-    if (segundosTotales === 0) {
-      clearInterval(intervalo);
+
+    if (segundosTotales > 0) {
+      pTiempoRestante.textContent = `${horas}:${minutos}:${segundos}`;
     }
+    if (segundosTotales <= 0) {
+      clearInterval(intervalo);
+      iniciarBoton.disabled = false;
+    }
+    pausarBoton.addEventListener("click", () => {
+      clearInterval(intervalo);
+      tiempoPausado = segundosTotales;
+      iniciarBoton.disabled = false;
+    });
   }, 1000);
+});
+
+iniciarBoton.addEventListener("click", () => {
+  console.log("Entré a este ");
+  if (tiempoPausado > 0) {
+    segundosTotales = tiempoPausado;
+    tiempoPausado = 0;
+    intervalo = setInterval(() => {
+      segundosTotales--;
+
+      iniciarBoton.disabled = true;
+
+      let horas = Math.floor(segundosTotales / 3600);
+      let minutos = Math.floor((segundosTotales % 3600) / 60);
+      let segundos = segundosTotales % 60;
+
+      horas = horas < 10 ? "0" + horas : horas;
+      minutos = minutos < 10 ? "0" + minutos : minutos;
+      segundos = segundos < 10 ? "0" + segundos : segundos;
+
+      if (segundosTotales > 0) {
+        pTiempoRestante.textContent = `${horas}:${minutos}:${segundos}`;
+      }
+
+      if (segundosTotales <= 0) {
+        clearInterval(intervalo);
+        iniciarBoton.disabled = false;
+      }
+      pausarBoton.addEventListener("click", () => {
+        clearInterval(intervalo);
+        tiempoPausado = segundosTotales;
+        iniciarBoton.disabled = false;
+      });
+    }, 1000);
+  }
 });
